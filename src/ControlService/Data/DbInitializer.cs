bbf77e889b80faaabe2091a6f544cdd5677822fb
Andreas Frank <andreas.frank1971@t-online.de>
2024-03-16 20:33:02 +0100

    InitCommit

---
+++ src/ControlService/Data/DbInitializer.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace ControlService.Entities;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("ControlDb", MongoClientSettings
+            .FromConnectionString(app.Configuration
+            .GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Control>()
+           .Key(c => c.Name, KeyType.Ascending) // Definiert das Feld, für das der Index erstellt werden soll
+           .Option(o => o.Unique = true) // Stellt sicher, dass der Index einzigartig ist
+           .CreateAsync();
+
+        await DB.Index<Tenant>()
+           .Key(c => c.TenantId, KeyType.Ascending) // Definiert das Feld, für das der Index erstellt werden soll
+           .Option(o => o.Unique = true) // Stellt sicher, dass der Index einzigartig ist
+           .CreateAsync();
+
+        var count = await DB.CountAsync<Control>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("No controls data - will attempt to seed");
+
+            //Controls seeden
+            var itemData = await File.ReadAllTextAsync("Data/controls.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var items = JsonSerializer.Deserialize<List<Control>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+
+
+        count = await DB.CountAsync<Tenant>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("No tenants data - will attempt to seed");
+
+            //Tenants seeden
+            var itemData = await File.ReadAllTextAsync("Data/tenants.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var items = JsonSerializer.Deserialize<List<Tenant>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+    }
+}
---
+++ src/ControlService/Model/Control.cs
@@ -0,0 +1,112 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Entities;
+
+public class Control : Entity
+{
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public List<Item> Items { get; set; }
+
+    public int Dependence { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public string Target { get; set; }
+
+    public DateTime? IsDeleted { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string AddBy { get; set; }
+    public DateTime AddDate { get; set; } = DateTime.UtcNow;
+
+    public string EditBy { get; set; }
+    public DateTime? EditDate { get; set; }
+}
+
+public class Item : Entity
+{
+    public List<Tenant> Tenants { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; }
+
+    public bool Aktiv { get; set; }
+
+    public int Index { get; set; }
+
+    public int RowIndex { get; set; }
+
+
+    [StringLength(450)]
+    public string Bemerkung { get; set; }
+
+
+    [StringLength(255)]
+    public string ImageName { get; set; }
+
+    public List<Sprache> Sprachen { get; set; }
+    public List<Parameter> Parameter { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string AddBy { get; set; }
+    public DateTime AddDate { get; set; } = DateTime.UtcNow;
+
+    public string EditBy { get; set; }
+    public DateTime? EditDate { get; set; }
+}
+
+public class Sprache
+{
+    [Required]
+    [StringLength(2)]
+    public string ShortCode { get; set; }
+
+    [Required]
+    [StringLength(5)]
+    public string FullCode { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Value1 { get; set; }
+
+    [StringLength(450)]
+    public string Beschreibung { get; set; }
+}
+
+public class Parameter
+{
+    public DateTime? IsDeleted { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Value { get; set; }
+
+    [StringLength(450)]
+    public string Bemerkung { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string AddBy { get; set; }
+    public DateTime AddDate { get; set; } = DateTime.UtcNow;
+}
+
+public class Tenant : Entity
+{
+    [Required]
+    public string TenantId { get; set; }
+
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+
+    // Weitere Eigenschaften je nach Bedarf
+}
